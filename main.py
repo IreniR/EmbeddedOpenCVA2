397f0c8decc204bd2b012932d045733cc07a40bc
IreniR <88494227+IreniR@users.noreply.github.com>
2021-11-07 15:43:37 -0500

    Updated to print

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import cv2
-import numpy as np
 
-capture = cv2.VideoCapture('C:\\Users\\ireni\\Downloads\\pexels-kelly-lacy-9331548.mp4')
+capture = cv2.VideoCapture('Pexels Videos 1572432.mp4')
+
 ret, frame1 = capture.read()
 ret, frame2 = capture.read()
 
@@ -11,11 +11,19 @@ while capture.isOpened():
     blur = cv2.GaussianBlur(gray, (5, 5), 0)
     _, thresh = cv2.threshold(blur, 20, 255, cv2.THRESH_BINARY)
     dilated = cv2.dilate(thresh, None, iterations=3)
-
     contours, _ = cv2.findContours(dilated, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    cv2.drawContours(frame1, contours, -1, (0, 255, 0), 2)
+
+    for contour in contours:
+        (x, y, w, h) = cv2.boundingRect(contour)
+
+        if cv2.contourArea(contour) < 600:
+            continue
+        cv2.rectangle(frame1, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        print("Object detected at: " + str(capture.get(cv2.CAP_PROP_POS_MSEC)))
 
     cv2.imshow("feed", frame1)
+    frame1 = frame2
+    ret, frame2 = capture.read()
 
     if cv2.waitKey(40) == 27:
         break
